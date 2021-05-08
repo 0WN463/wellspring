cdcb8582c3c1bab62b10caa8eff7bb1b66604bff
0WN463 <E0175456@u.nus.edu>
2021-05-08 11:50:20 +0800

    Add common modules

---
+++ common/utility.py
@@ -0,0 +1,15 @@
+import inspect
+def show_implementation(cls, section=None):
+    SECTION_DELIMITER = "Section "
+    found_section = False
+    it = inspect.getsourcelines(cls)[0]
+    print(it[0])
+    for line in it[1:]:
+        if section is not None and section not in line and not found_section:
+            continue
+        found_section = True
+        
+        if SECTION_DELIMITER in line and section is not None and section not in line:
+            break
+        print(line, end='')
+        
