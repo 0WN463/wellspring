78446f5b0c73370df58a2997f250d9a49058ab5b
0WN463 <>
2021-06-11 00:38:03 +0800

    Clean up computer-vision introduction

--- common/utility.py
+++ common/utility.py
@@ -1,4 +1,6 @@
 import inspect
+import matplotlib.pyplot as plt
+
 def show_implementation(cls, section=None):
     SECTION_DELIMITER = "Section "
     found_section = False
@@ -13,3 +15,24 @@ def show_implementation(cls, section=None):
             break
         print(line, end='')
         
+def show_images(img_label_tuples, grey=True, autoscale=False):
+    n = len(img_label_tuples)
+    f = plt.figure(figsize=(20,10))
+    
+    axes = [f.add_subplot(100 + 10 * n + i) for i in range(1, n+1)]
+    
+    vmax = 1 if all([image.max() <= 1 for image, _ in img_label_tuples]) else 255
+    
+
+    for ax, (image, label) in zip(axes, img_label_tuples):
+        ax.title.set_text(label)
+        if autoscale:
+            vmax = image.max()
+        if grey:
+            ax.imshow(image, cmap='gray', vmin=0, vmax=vmax)
+        else:
+            ax.imshow(image)
+            
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+
