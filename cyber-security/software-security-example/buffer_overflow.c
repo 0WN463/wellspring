8d0475b60d4295e53a048f7e959bf4fcd787efc5
0WN463 <>
2021-07-14 00:25:10 +0800

    Complete chapter on software security

--- cyber-security/software-security-example/buffer_overflow.c
+++ cyber-security/software-security-example/buffer_overflow.c
@@ -3,14 +3,16 @@
 void change_value(char index, char value) {
 	char arr[10];
 	int b = 100;
-
 	printf("The value of b is %d\n", b);	
 	printf("Changing index %d of a to %d\n", index, value);
+	printf("The return address is %p\n", __builtin_return_address(0));
 	
 	arr[index] = value;
+	
 	printf("The value of b is %d\n", b);	
-	printf("Changing index %d of a to %d\n", index, value);
+	printf("The return address is %p\n", __builtin_return_address(0));
 }
+
 
 int main(int argc, char *argv[])  {
 	if(argc != 3) {
