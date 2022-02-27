d3c7c9848a509096f5827af12ac35dff3a9374d8
0WN463 <>
2022-02-28 00:49:34 +0800

    Cleanup various chapters in algorithm analysis

--- algorithm-analysis/module/sort.py
+++ algorithm-analysis/module/sort.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def _merge(arr1, arr2):
     i, j = 0, 0
     arr = [None for _ in arr1 + arr2]
@@ -27,3 +29,30 @@ def merge_sort(A):
     arr1 = merge_sort(A[:m]) # T(floor(n/2))
     arr2 = merge_sort(A[m:]) # T(ceil(n/2))
     return _merge(arr1, arr2) # Theta(n)
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    index = randint(0, len(arr) - 1) 
+    left, right = _partition(arr, index)
+    left = quick_sort(left)
+    right = quick_sort(right)
+    return left + [arr[index]] + right
+
+
+def _partition(arr, index):
+    pivot = arr[index]
+    return [e for i, e in enumerate(arr) if e <= pivot and i != index], [e for e in arr if e > pivot]
+
+def quick_select(arr, r):
+    index = randint(0, len(arr) - 1) 
+    left, right = _partition(arr, index)
+    if len(left) == r:
+        return arr[index]
+
+    if r > len(left):
+        return quick_select(right, r-len(left)-1)
+    
+    return quick_select(left, r)
