ec12f6112df498224b8f9a6b9197e3ef07fabeb4
0WN463 <>
2022-03-30 01:00:10 +0800

    Fix proof for triangle Fourier series

--- signal_processing/module/wave.py
+++ signal_processing/module/wave.py
@@ -1,5 +1,12 @@
 import numpy as np
+from . import signal
 
 
 def square(ts, T, A):
     return np.where(np.floor((ts + T/2) / T) % 2 == 0, A, 0)
+
+
+def tri(ts, T, A):
+    factor = np.floor((ts + T/2)/T)
+    xs = ts - T * factor
+    return A * signal.tri(xs, T/2) * np.where(factor % 2 == 0, 1, -1)
