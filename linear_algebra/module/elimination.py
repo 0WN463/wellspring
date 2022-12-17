e7560da13b0622117ba0cdc44f6e4bb31ab7ffb2
0WN463 <>
2022-12-17 18:09:01 +0800

    Fix GE and GJE functions and cleanup notebooks

--- linear_algebra/module/elimination.py
+++ linear_algebra/module/elimination.py
@@ -1,35 +1,48 @@
 import numpy as np
+from fractions import Fraction
+from typing import Optional
+import numpy.typing as npt
 
+arr_type = npt.NDArray[np.int_]
+frac_arr_type = npt.NDArray[np.object_]
 
-def gaussian_elim(arr: np.ndarray, b: np.ndarray = None) -> np.ndarray:
-    _b = np.zeros((arr.shape[0], 1)) if b is None else b
+def gaussian_elim(arr: arr_type, b: Optional[arr_type] = None) -> frac_arr_type|tuple[frac_arr_type, frac_arr_type]:
+    _b:arr_type = np.zeros((arr.shape[0], 1)).astype('int') if b is None else b
+    arr = arr + Fraction()
+    _b = _b + Fraction()
 
-    if 1 in arr.shape:
+    if 0 in arr.shape:
         return (arr, _b) if b is not None else arr
 
-    arr = arr.astype('float')
-    _b = _b.astype('float')
+    r = 0
+    for c in range(arr.shape[1]):
+        if np.all(arr[:,c] == 0):
+            continue
+        if arr[r][c] == 0:
+            indices = np.flatnonzero(arr[:, c])
+            indices = indices[indices > r]
+            if len(indices) == 0:
+                continue
+            i = indices[0]
 
-    try:
-        col = np.argwhere(np.any(arr != 0, axis=0)).ravel()[0]
-    except IndexError:
-        return (arr, _b) if b is not None else arr
-    if arr[0][col] == 0:
-        i = np.argwhere(arr[:, col] != 0).ravel()[0]
-        arr[i], arr[0] = arr[0].copy(), arr[i].copy()
-        _b[i], _b[0] = _b[0].copy(), _b[i].copy()
-    for i in range(1, arr.shape[0]):
-        factor = arr[i][col] / arr[0][col]
-        arr[i] -= arr[0] * factor
-        _b[i] -= _b[0] * factor
+            arr[i], arr[r] = arr[r].copy(), arr[i].copy()
+            _b[i], _b[r] = _b[r].copy(), _b[i].copy()
 
-    arr[1:, 1:], _b[1:, :] = gaussian_elim(arr[1:, 1:], _b[1:, :])
+        #arr[r] /= arr[r][c]
+        #factors = arr[:, c].copy() 
+        factors = arr[:, c].copy() / arr[r][c]
+        factors[:r+1] = 0
+        dx = np.tile(arr[r], (arr.shape[0], 1)) * factors[:,None]
+        arr -= dx
+
+        dx = np.tile(_b[r], (_b.shape[0], 1)) * factors[:,None]
+        _b -= dx
+        r += 1
 
     return (arr, _b) if b is not None else arr
 
-
-def gauss_jordan_elim(arr: np.ndarray, b: np.ndarray = None) -> np.ndarray:
-    _b = np.zeros((arr.shape[0], 1)) if b is None else b
+def gauss_jordan_elim(arr: arr_type, b: Optional[arr_type] = None) -> frac_arr_type|tuple[frac_arr_type, frac_arr_type]:
+    _b = np.zeros((arr.shape[0], 1)).astype('int') if b is None else b
     arr, _b = gaussian_elim(arr, _b)
     
     for i in range(arr.shape[0]):
@@ -41,9 +54,11 @@ def gauss_jordan_elim(arr: np.ndarray, b: np.ndarray = None) -> np.ndarray:
         _b[i] /= arr[i][col]
         arr[i] /= arr[i][col]
 
+
         for j in range(i):
             factor = arr[j][col]
             arr[j] -= arr[i] * factor
             _b[j] -= _b[i] * factor
 
     return (arr, _b) if b is not None else arr
+
--- linear_algebra/module/matrix.py
+++ linear_algebra/module/matrix.py
@@ -5,7 +5,7 @@ from .elimination import gauss_jordan_elim
 def mult(A: np.ndarray, B: np.ndarray) -> np.ndarray:
     assert A.shape[1] == B.shape[0]
 
-    result = np.zeros((A.shape[0], B.shape[1]))
+    result = np.zeros((A.shape[0], B.shape[1])).astype('object')
 
     for i in range(A.shape[0]):
         for j in range(B.shape[1]):
@@ -17,7 +17,7 @@ def inv(A: np.ndarray) -> np.ndarray | None:
     assert A.ndim == 2
     assert A.shape[0] == A.shape[1]
 
-    I = np.identity(A.shape[0])
+    I = np.identity(A.shape[0]).astype('int')
 
     R, B = gauss_jordan_elim(A, I)
 
